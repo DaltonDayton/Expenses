eabcb795308679d08c855381560c5cfe0f24ba09
Dalton Dayton <daltondayton1@gmail.com>
2023-06-21 22:06:22 -0600

    Add State

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,17 +1,22 @@
+import { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = ({ expenses }) => {
+  const [title, setTitle] = useState(expenses.title);
+
   const clickHandler = () => {
-    console.log("Clicked!!!");
+    setTitle("Updated!");
+    console.log(title);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={expenses.date} />
       <div className="expense-item__description">
-        <h2>{expenses.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${expenses.amount.toFixed(2)}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
