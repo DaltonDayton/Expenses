ca89bf59a37e37084836b277e37f4ee7550a8357
Dalton Dayton <daltondayton1@gmail.com>
2023-06-22 22:31:50 -0600

    My attempt

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
+import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 const ExpenseItemList = (props) => {
+  const [year, setYear] = useState(new Date().getFullYear().toString());
+  const dropdownChangeHandler = (enteredYear) => {
+    setYear(enteredYear);
+    console.log(year);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter onDropdownChange={dropdownChangeHandler} />
       <ExpenseItem expenses={props.expenses[0]} />
       <ExpenseItem expenses={props.expenses[1]} />
       <ExpenseItem expenses={props.expenses[2]} />
