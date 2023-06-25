e87e5bc10e80a3ba27f92dee146616d48963caed
Dalton Dayton <daltondayton1@gmail.com>
2023-06-25 12:53:02 -0600

    instructor resolution

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -5,18 +5,15 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState(2020);
-  const [filteredExpenses, setFilteredExpenses] = useState(props.items);
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    setFilteredExpenses(props.items);
-    setFilteredExpenses((filteredExpenses) => {
-      return filteredExpenses.filter(
-        (expense) => expense.date.getFullYear().toString() === selectedYear
-      );
-    });
   };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <Card className="expenses">
